e496c8e0222c169fbacb764686ba1e98464b9938
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2025-12-03 13:55:07 -0500

    Add parser benchmarks

--- 99-FFI-Shapes-Refactor/Example-6/shapes_py/benches/bench_shape_parser.py
+++ 99-FFI-Shapes-Refactor/Example-6/shapes_py/benches/bench_shape_parser.py
@@ -1,4 +1,5 @@
 import copy
+import random
 
 import pytest
 from hamcrest import *
@@ -34,3 +35,42 @@ def bench_parse_shape_line_failure(benchmark, line: str):
             pass
 
     benchmark(wrapped)
+
+
+@pytest.mark.parametrize("num_lines", [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000])
+def bench_parse_shape_file(benchmark, num_lines: int):
+    KNOWN_SHAPES = [
+        ("Triangle", 3),
+        ("Right Triangle", 2),
+        ("Equilateral Triangle", 1),
+        ("Square", 1),
+        ("Circle", 1)
+    ]
+
+    INVALID_SHAPE_RATE = 0.10
+    INVALID_DIM_RATE = 0.10
+
+    num_valid_shapes = 0
+
+    random_shape_filename = f"random-shapes-{num_lines}.txt"
+    with open(random_shape_filename, "w") as random_shape_file:
+        for _ in range(num_lines):
+            # Create an unknown shape"
+            if random.random() < INVALID_SHAPE_RATE:
+                random_shape_file.write("LOL Not a Shape; 1 2 3 4 5\n")
+                continue
+
+            shape, dim_count = random.choice(KNOWN_SHAPES)
+
+            dims = " ".join((f"{random.uniform(1, 10)}" for _ in range(dim_count)))
+
+            random_shape_file.write(f"{shape}; {dims}\n")
+
+            num_valid_shapes += 1
+
+    benchmark(ShapeParser.read_shapes, random_shape_filename)
+
+
+
+
+
