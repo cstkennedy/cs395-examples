3168a9b0ae2ed50ebbc5bd93dd780f4b9f989810
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2026-06-15 12:42:09 -0400

    Start new Rust Refactoring discussion

---
+++ 06-Rust-Shapes-Refactor/Example-1/shapes/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod circle;
+pub mod equilateral_triangle;
+pub mod right_triangle;
+pub mod shape;
+pub mod square;
+pub mod triangle;
+
+pub mod factory;
+pub mod parser;
+
+pub mod prelude {
+    pub use crate::factory::Factory;
+    pub use crate::parser::Parser;
+    pub use crate::shape::Shape;
+}
