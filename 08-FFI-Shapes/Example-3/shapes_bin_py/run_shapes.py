4e27983b250321efa174f8bf93f4d0eefd3a2f93
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2026-07-01 10:54:40 -0400

    Add Error Transmutation to FFI examples

---
+++ 08-FFI-Shapes/Example-3/shapes_bin_py/run_shapes.py
@@ -0,0 +1,110 @@
+import logging
+import sys
+from typing import Final, Generator, Optional, TextIO
+
+from headings import BorderHeading, MultiLineBorderHeading
+from shapes.shape import Shape
+from shapes_py import ShapeFactory
+
+PROGRAM_HEADING = MultiLineBorderHeading(
+    content=(
+        "Objects & Inheritance: 2-D Shapes",
+        "Thomas J. Kennedy",
+    ),
+    width=80,
+    symbol="-",
+)
+
+FACTORY_DESCRIPTION: Final[str] = "\n".join(
+    (
+        "~" * 38,
+        "Available Shapes".center(38),
+        "~" * 38,
+        ShapeFactory.list_known(),
+        "-" * 38,
+        f"{ShapeFactory.number_known():>2} shapes available.\n",
+    )
+)
+
+
+def read_shape(line: str) -> Shape | None:
+    """
+    T.B.W.
+    """
+
+    # Split on ";" and Strip leading/trailing whitespace
+    # And Unpack the list
+    name, values = [part.strip() for part in line.split(";")]
+
+    values = values.strip()
+
+    values = [float(val) for val in values.split()]
+    shape = ShapeFactory.create_with(name, values)
+
+    return shape
+
+
+def read_shapes(shapes_in: TextIO) -> Generator[Shape | None, None, None]:
+    """
+    T.B.W.
+    """
+
+    for line in shapes_in:
+        try:
+            yield read_shape(line)
+
+        except KeyError as _err:
+            logging.warning(f"Skipped {line=!r:} due to unknown shape.")
+
+        except ValueError as _err:
+            logging.warning(f"Skipped shape {line=!r:} due to malformed line.")
+
+
+def main() -> None:
+    """
+    The main function. In practice I could name this
+    anything. The name main was selected purely
+    out of familiarity.
+
+    The "if __name__" line below determines what runs
+    """
+
+    if len(sys.argv) < 2:
+        print("No input file provided.")
+        print(f"Usage: {sys.argv[0]:} input_file")
+        sys.exit(1)
+
+    shapes_filename = sys.argv[1]
+
+    print(PROGRAM_HEADING)
+    print(FACTORY_DESCRIPTION)
+
+    with open(shapes_filename, "r") as shapes_in:
+        shapes = [shp for shp in read_shapes(shapes_in) if shp is not None]
+
+    print(BorderHeading("Display All Shapes"))
+    for shp in shapes:
+        print(shp)
+        print()
+
+    print(BorderHeading("Display Largest Shape (Area)"))
+    largest_shape = max(shapes, key=lambda shape: shape.area())
+    print(largest_shape)
+    print()
+
+    print(BorderHeading("Display Smallest Shape (Perimeter)"))
+    smallest_shape = min(shapes, key=lambda shape: shape.perimeter())
+    print(smallest_shape)
+    print()
+
+    print(BorderHeading("Display Shapes Sorted by Name"))
+    for shp in sorted(shapes, key=lambda shape: shape.name):
+        print(shp)
+        print()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except FileNotFoundError as err:
+        print(err)
---
+++ 08-FFI-Shapes/Example-3/shapes_py/src/lib.rs
@@ -0,0 +1,46 @@
+use pyo3::prelude::*;
+
+mod circle;
+mod square;
+mod triangle;
+mod factory;
+
+use crate::circle::CircleWrapper;
+use crate::square::SquareWrapper;
+use crate::triangle::{EquilateralTriangleWrapper, RightTriangleWrapper, TriangleWrapper};
+use crate::factory::{ShapeWrapper, ShapeFactory};
+
+#[pymodule]
+mod shapes_py {
+    use super::*;
+
+    #[pymodule_export]
+    use CircleWrapper;
+
+    #[pymodule_export]
+    use SquareWrapper;
+
+    #[pymodule_export]
+    use TriangleWrapper;
+
+    #[pymodule_export]
+    use EquilateralTriangleWrapper;
+
+    #[pymodule_export]
+    use RightTriangleWrapper;
+
+    #[pymodule_export]
+    use ShapeWrapper;
+
+    #[pymodule_export]
+    use ShapeFactory;
+
+    /*
+    #[pymodule_init]
+    pub fn init(m: &Bound<'_, PyModule>) -> PyResult<()> {
+        pyo3_log::init();
+
+        Ok(())
+    }
+    */
+}
